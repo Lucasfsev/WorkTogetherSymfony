00ff88de2aac1efadac60f813f1b426e3c441455
lucas <lucas.alleaume@iia-formation.fr>
2025-02-19 19:17:21 +0100

    Clean Migration

---
+++ migrations/Version20250219181410.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20250219181410 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE bay (id INT AUTO_INCREMENT NOT NULL, reference VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE intervention (id INT AUTO_INCREMENT NOT NULL, unit_id INT NOT NULL, type_id INT NOT NULL, start_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, INDEX IDX_D11814ABF8BD700D (unit_id), INDEX IDX_D11814ABC54C8C93 (type_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE offer (id INT AUTO_INCREMENT NOT NULL, promotion_percentage SMALLINT NOT NULL, unit_limit SMALLINT DEFAULT NULL, name VARCHAR(100) NOT NULL, available TINYINT(1) NOT NULL, price NUMERIC(10, 2) NOT NULL, description LONGTEXT DEFAULT NULL, image_path VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE `order` (id INT AUTO_INCREMENT NOT NULL, offer_id INT NOT NULL, customer_id INT NOT NULL, start_date DATE NOT NULL, end_date DATE DEFAULT NULL, unit_price DOUBLE PRECISION NOT NULL, INDEX IDX_F529939853C674EE (offer_id), INDEX IDX_F52993989395C3F3 (customer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE setting (id INT AUTO_INCREMENT NOT NULL, setting_key VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE state_unit (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, color VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type_intervention (id INT AUTO_INCREMENT NOT NULL, reference VARCHAR(255) NOT NULL, color VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE type_unit (id INT AUTO_INCREMENT NOT NULL, reference VARCHAR(100) NOT NULL, color VARCHAR(10) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE unit (id INT AUTO_INCREMENT NOT NULL, state_id INT NOT NULL, type_id INT NOT NULL, bay_id INT NOT NULL, reference VARCHAR(10) NOT NULL, INDEX IDX_DCBB0C535D83CC1 (state_id), INDEX IDX_DCBB0C53C54C8C93 (type_id), INDEX IDX_DCBB0C53DF9BA23B (bay_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE unit_order (unit_id INT NOT NULL, order_id INT NOT NULL, INDEX IDX_7E1BED93F8BD700D (unit_id), INDEX IDX_7E1BED938D9F6D38 (order_id), PRIMARY KEY(unit_id, order_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, firstname VARCHAR(255) DEFAULT NULL, lastname VARCHAR(255) DEFAULT NULL, password VARCHAR(255) NOT NULL, mail_address VARCHAR(255) NOT NULL, role VARCHAR(255) NOT NULL, is_verified TINYINT(1) NOT NULL, discr VARCHAR(255) NOT NULL, billing_address VARCHAR(255) DEFAULT NULL, post_code VARCHAR(10) DEFAULT NULL, town VARCHAR(255) DEFAULT NULL, country VARCHAR(170) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT FK_D11814ABF8BD700D FOREIGN KEY (unit_id) REFERENCES unit (id)');
+        $this->addSql('ALTER TABLE intervention ADD CONSTRAINT FK_D11814ABC54C8C93 FOREIGN KEY (type_id) REFERENCES type_intervention (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F529939853C674EE FOREIGN KEY (offer_id) REFERENCES offer (id)');
+        $this->addSql('ALTER TABLE `order` ADD CONSTRAINT FK_F52993989395C3F3 FOREIGN KEY (customer_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE unit ADD CONSTRAINT FK_DCBB0C535D83CC1 FOREIGN KEY (state_id) REFERENCES state_unit (id)');
+        $this->addSql('ALTER TABLE unit ADD CONSTRAINT FK_DCBB0C53C54C8C93 FOREIGN KEY (type_id) REFERENCES type_unit (id)');
+        $this->addSql('ALTER TABLE unit ADD CONSTRAINT FK_DCBB0C53DF9BA23B FOREIGN KEY (bay_id) REFERENCES bay (id)');
+        $this->addSql('ALTER TABLE unit_order ADD CONSTRAINT FK_7E1BED93F8BD700D FOREIGN KEY (unit_id) REFERENCES unit (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE unit_order ADD CONSTRAINT FK_7E1BED938D9F6D38 FOREIGN KEY (order_id) REFERENCES `order` (id) ON DELETE CASCADE');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY FK_D11814ABF8BD700D');
+        $this->addSql('ALTER TABLE intervention DROP FOREIGN KEY FK_D11814ABC54C8C93');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F529939853C674EE');
+        $this->addSql('ALTER TABLE `order` DROP FOREIGN KEY FK_F52993989395C3F3');
+        $this->addSql('ALTER TABLE unit DROP FOREIGN KEY FK_DCBB0C535D83CC1');
+        $this->addSql('ALTER TABLE unit DROP FOREIGN KEY FK_DCBB0C53C54C8C93');
+        $this->addSql('ALTER TABLE unit DROP FOREIGN KEY FK_DCBB0C53DF9BA23B');
+        $this->addSql('ALTER TABLE unit_order DROP FOREIGN KEY FK_7E1BED93F8BD700D');
+        $this->addSql('ALTER TABLE unit_order DROP FOREIGN KEY FK_7E1BED938D9F6D38');
+        $this->addSql('DROP TABLE bay');
+        $this->addSql('DROP TABLE intervention');
+        $this->addSql('DROP TABLE offer');
+        $this->addSql('DROP TABLE `order`');
+        $this->addSql('DROP TABLE setting');
+        $this->addSql('DROP TABLE state_unit');
+        $this->addSql('DROP TABLE type_intervention');
+        $this->addSql('DROP TABLE type_unit');
+        $this->addSql('DROP TABLE unit');
+        $this->addSql('DROP TABLE unit_order');
+        $this->addSql('DROP TABLE user');
+    }
+}
