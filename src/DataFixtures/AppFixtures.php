003b05ae823a47ce37f1c1e86f8a06abc4539e89
lucas <lucas.alleaume@iia-formation.fr>
2025-02-20 20:04:52 +0100

    Fix Roles Array & Role String + Register working

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,9 +15,15 @@ use App\Entity\User;
 use DateTimeImmutable;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasherInterface) {
+        $this->hasher = $userPasswordHasherInterface;
+    }
     public function load(ObjectManager $manager): void
     {
         $states = [
@@ -117,6 +123,8 @@ class AppFixtures extends Fixture
             $newCustomer->setMailAddress($customer[2]);
             $newCustomer->setPassword($customer[3]);
             $newCustomer->setRole("ROLE_CLIENT");
+            $password = $this->hasher->hashPassword($newCustomer, $customer[3]);
+            $newCustomer->setPassword($password);
             array_push($currentCustomers,$newCustomer);
             $manager->persist($newCustomer);
         }
